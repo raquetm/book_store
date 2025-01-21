8a0fde298dece6bb0554835283e4ce29cf68a491
raquetm <a23raqueltm@iessanclemente.net>
2025-01-21 11:50:23 +0100

    finalizado

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,10 +4,37 @@ from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    def __str__(self):
+        return f'{self.name} {self.code}'
+    
+    class Meta():
+        verbose_name_plural = "Countries"
+
+class Address(models.Model):
+    street = models.CharField(max_length=250)
+    postalCode = models.CharField(max_length=10)
+    city = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'Street : {self.street} - postal code : {self.postalCode} - city : {self.city}'
+    
+    class Meta():
+        verbose_name_plural = "Address Entries"
+
+
 class Author(models.Model):
   first_name=models.CharField(max_length=100)
   last_name=models.CharField(max_length=100)
-  
+  address = models.OneToOneField(Address,on_delete=models.CASCADE,null = True)
+    def fullName(self):
+        return f'{self.first_name} {self.last_name}' 
+
+    def __str__(self):
+        return self.fullName()  
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
@@ -15,9 +42,11 @@ class Book(models.Model):
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
-    # def save(self,*args,**kwargs):
-    #     self.slug=slugify(self.title)
-    #     super().save(*args,**kwargs)    
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.title)
+        super().save(*args,**kwargs)
+    
+    
     def __str__(self):
         return f'{self.title}  ({self.rating} author = {self.author} and bestselling={self.is_bestselling}) {self.slug}'
     def get_absolute_url(self):
